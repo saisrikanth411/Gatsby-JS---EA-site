9aeba67b9630a2ef1a9c7426940fd2e37db69cae
SAI SRIKANTH KURUMADDALI <47223769+saisrikanth411@users.noreply.github.com>
2020-09-13 22:25:08 -0400

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [],
+}
+
+// const path = require(`path`)
+
+// module.exports = {
+//   plugins: [
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `images`,
+//         path: path.join(__dirname, `src`, `images`),
+//       },
+//     },
+//     `gatsby-plugin-sharp`,
+//     `gatsby-transformer-sharp`,
+//   ],
+// }
